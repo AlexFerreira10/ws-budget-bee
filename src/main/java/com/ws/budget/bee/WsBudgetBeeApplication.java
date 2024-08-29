6647fff757276dfa8be4750101ff62d618016924
AlexFerreira10 <correaferreiraalex92@email.com>
2024-08-29 12:30:46 -0300

    Initial commit

---
+++ src/main/java/com/ws/budget/bee/WsBudgetBeeApplication.java
@@ -0,0 +1,13 @@
+package com.ws.budget.bee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBudgetBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBudgetBeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ws/budget/bee/WsBudgetBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ws.budget.bee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBudgetBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
